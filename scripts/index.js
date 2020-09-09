ccdb45b01238ec866ac017c86240a4fcafab067a
Pete Looney <petelooney81@gmail.com>
2020-09-09 13:27:10 -0500

    finished the dom-102 exercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-
+const container = document.querySelector('.js-link-container');
 
 const addresses = [
     "https://google.com",
@@ -6,3 +6,10 @@ const addresses = [
     "https://duckduckgo.com"
 ];
 
+for (let index = 0; index < addresses.length; index++) {
+    let newAnchor = document.createElement('a');
+    newAnchor.setAttribute('href', addresses[index]);
+    newAnchor.setAttribute('target', '_blank')
+    newAnchor.textContent = "click here"
+    container.appendChild(newAnchor)
+}
